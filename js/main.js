9f68623e316a071e9a627b982fa501100bd694ae
Noah Yarian <noah.yarian@gmail.com>
2015-06-21 16:51:32 -0500

    publish: updated readme
    generated from commit 2badf1272837bc1501196b917803ef6ced41d3da

---
+++ js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var app = angular.module('goDo', ['ngRoute']).config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'views/landing.html',
+    controller: 'mainController',
+    controllerAs: 'main'
+  }).when('/happening', {
+    templateUrl: 'views/happening.html'
+  }).when('/profile', {
+    templateUrl: 'views/profile.html'
+  }).when('/login', {
+    templateUrl: 'views/login.html'
+  });
+}).controller('mainController', function () {
+  console.log('hola');
+});
