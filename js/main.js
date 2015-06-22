0457563ba9cae285c8a174b09adbb9ea012fbcd3
Noah Yarian <noah.yarian@gmail.com>
2015-06-22 08:47:15 -0500

    publish: cleaned up for deploy
    generated from commit e99f8a9e562ac6ef71db0b9cda03795a4b91f26b

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 'use strict';
 
-var app = angular.module('goDo', ['ngRoute']).config(function ($routeProvider) {
+var app = angular.module('goDo', ['ngRoute', 'firebase']).config(function ($routeProvider) {
   $routeProvider.when('/', {
-    templateUrl: 'views/landing.html',
-    controller: 'mainController',
-    controllerAs: 'main'
+    templateUrl: 'views/landing.html'
   }).when('/happening', {
     templateUrl: 'views/happening.html'
   }).when('/profile', {
@@ -12,6 +10,36 @@ var app = angular.module('goDo', ['ngRoute']).config(function ($routeProvider) {
   }).when('/login', {
     templateUrl: 'views/login.html'
   });
-}).controller('mainController', function () {
-  console.log('hola');
+}).controller('LandingCtrl', function ($scope, $firebaseObject) {
+  var ref = new Firebase('https://goanddo.firebaseio.com/data');
+  // download the data into a local object
+  var syncObject = $firebaseObject(ref);
+  // synchronize the object with a three-way data binding
+  // click on `index.html` above to see it used in the DOM!
+  syncObject.$bindTo($scope, 'data');
+}).controller('ArrayCtrl', function ($scope, $firebaseArray) {
+  var ref = new Firebase('https://goanddo.firebaseio.com/messages');
+  // create a synchronized array
+  // click on `index.html` above to see it used in the DOM!
+  $scope.messages = $firebaseArray(ref);
+
+  // add new items to the array
+  // the message is automatically added to our Firebase database!
+  $scope.addMessage = function () {
+    $scope.messages.$add({
+      text: $scope.newMessageText
+    });
+  };
+}).controller('FBCtrl', function ($scope, $firebaseAuth) {
+  var vm = this;
+  vm.login = function () {
+    vm.ref = new Firebase('https://goanddo.firebaseio.com');
+    vm.ref.authWithOAuthPopup('facebook', function (error, authData) {
+      if (error) {
+        console.log('Login Failed!', error);
+      } else {
+        console.log('Authenticated successfully with payload:', authData);
+      }
+    });
+  };
 });
