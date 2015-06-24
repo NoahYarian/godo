e33c8043be87fe0b45543a432b7581e42bc04cb9
Noah Yarian <noah.yarian@gmail.com>
2015-06-24 14:59:58 -0500

    publish: updated readme, friends data is working (and overwriting other user data), new deploy script using open
    generated from commit 68cc9a1bec35f9dd0e26a732c6ccf9275b8ccd85

--- js/main.js
+++ js/main.js
@@ -31,37 +31,17 @@ var app = angular.module('goDo', ['ngRoute', 'firebase', 'ngFacebook']).config(f
 }).controller('FaceCtrl', function ($rootScope, $scope, $facebook, $firebaseObject) {
   $scope.login = function () {
     $facebook.login().then(function () {
-      $scope.getId();
-      setTimeout(function () {
-        $scope.refresh();
-        setTimeout(function () {
-          $scope.getFriends();
-        }, 2000);
-      }, 2000);
+      $scope.getMyInfo();
+      //$scope.getFriends();
     });
   };
-  $scope.getId = function () {
+  $scope.getMyInfo = function () {
     $facebook.api('/me').then(function (response) {
+      console.log('getMyInfo response: ', response);
       $rootScope.loggedInUser = response.id;
-    });
-  };
-  $scope.refresh = function () {
-    $facebook.api('/me').then(function (response) {
-      console.log('refresh response: ', response);
-      // $scope.resMe = response;
-      // $rootScope.loggedInUser = response.id;
-      console.log('refresh loggedInUser: ', $rootScope.loggedInUser);
-      var refMe = new Firebase('https://goanddo.firebaseio.com/users/' + $rootScope.loggedInUser + '/loginObj');
-      var objMe = $firebaseObject(refMe);
-      objMe.$loaded().then(function (objMe) {
-        objMe.loginInfo = response;
-      });
-      objMe.$save().then(function (refMe) {
-        refMe.key() === objMe.$id; // true
-      }, function (error) {
-        console.log('Error:', error);
-      });
-      console.log('Login obj: ', objMe);
+      console.log('getMyInfo loggedInUser: ', $rootScope.loggedInUser);
+      var ref = new Firebase('https://goanddo.firebaseio.com/users/' + $rootScope.loggedInUser);
+      ref.set({ loginObj: response });
       location.href = '/#/loggedin';
     }, function (err) {
       console.log('Facebook login issue...', err);
@@ -74,14 +54,14 @@ var app = angular.module('goDo', ['ngRoute', 'firebase', 'ngFacebook']).config(f
       console.log('getFriends loggedInUser: ', $rootScope.loggedInUser);
       var refFriends = new Firebase('https://goanddo.firebaseio.com/users/' + $rootScope.loggedInUser + '/friends');
       var objFriends = $firebaseObject(refFriends);
-      objFriends.$loaded().then(function (objFriends) {
-        objFriends = response;
-        objFriends.data.forEach(function (friend) {
-          console.log(friend.id, friend.name);
-          var id = friend.id;
-          objFriends[id] = true;
-        });
+      // objFriends.$loaded().then(function (objFriends) {
+      objFriends = response;
+      objFriends.data.forEach(function (friend) {
+        console.log(friend.id, friend.name);
+        var id = friend.id;
+        objFriends[id] = true;
       });
+      // });
       // obj.$save().then(function(ref) {
       //   ref.key() === obj.$id; // true
       // }, function(error) {
