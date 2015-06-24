9c6a6957dbefd4a005de2dc99e274502cb62934c
Noah Yarian <noah.yarian@gmail.com>
2015-06-24 18:24:02 -0500

    publish: saving user info and friends at login without overwriting firebase object is working
    generated from commit 7260f80e364282d40cb8902afec623306054ddfc

--- js/main.js
+++ js/main.js
@@ -128,51 +128,21 @@ var app = angular.module('goDo', ['ngRoute', 'firebase', 'ngFacebook']).config(f
   var ref = new Firebase('https://goanddo.firebaseio.com/users/' + $rootScope.loggedInUser + '/schedule');
   var syncObject = $firebaseObject(ref);
   syncObject.$bindTo($scope, 'data');
-})
-
-// .factory('srvAuth', function () {
-
-//   watchLoginChange = function() {
-//     var _self = this;
-//     FB.Event.subscribe('auth.authResponseChange', function(res) {
-//       if (res.status === 'connected') {
-//         /*
-//          The user is already logged,
-//          is possible retrieve his personal info
-//         */
-//         _self.getUserInfo();
-//         /*
-//          This is also the point where you should create a
-//          session for the current user.
-//          For this purpose you can use the data inside the
-//          res.authResponse object.
-//         */
-//       }
-//       else {
-//         /*
-//          The user is not logged to the app, or into Facebook:
-//          destroy the session on the server.
-//         */
-//       }
-//     });
-//   }
-
-//   getUserInfo = function() {
-//     var _self = this;
-//     FB.api('/me', function(res) {
-//       $rootScope.$apply(function() {
-//         $rootScope.user = _self.user = res;
-//       });
-//     });
-//   }
-
-//   logout = function() {
-//     var _self = this;
-//     FB.logout(function(response) {
-//       $rootScope.$apply(function() {
-//         $rootScope.user = _self.user = {};
-//       });
-//     });
-//   }
-// })
-;
+}).controller('EventCtrl', function ($scope, $rootScope, $firebase) {
+  $scope.getUserStartTimes = function (facebookId) {
+    var ref = new Firebase('https://goanddo.firebaseio.com/users/' + facebookId + '/schedule');
+    var hourBlocks = {};
+    ref.forEach(function (day, i) {
+      hourBlocks[day] = {};
+      day.forEach(function (halfHour, j) {
+        if (halfHour) {
+          hourBlocks[day][halfHour] = 0.5;
+          while (day[j + 1]) {
+            hourBlocks[day][halfHour] += 0.5;
+          }
+        }
+      });
+    });
+    console.log(hourBlocks);
+  };
+});
