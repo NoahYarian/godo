e15f7a9b128945f086487372e85c0aaf7010f1ca
Noah Yarian <noah.yarian@gmail.com>
2015-06-24 21:49:49 -0500

    publish: saving user info and friends at login without overwriting firebase object is working
    generated from commit 7260f80e364282d40cb8902afec623306054ddfc

--- js/main.js
+++ js/main.js
@@ -145,7 +145,7 @@ var app = angular.module('goDo', ['ngRoute', 'firebase', 'ngFacebook']).config(f
       week.forEach(function (day) {
         week2[day] = [];
         $.each(day, function (key, value) {
-          if (value) {
+          if (value === 'true') {
             week2[day].push(key);
           }
         });
